4fd37973cc1c8bdc777b851d3885fc05b096865c
Arturo Saldivar <damonbalam@gmail.com>
2023-02-17 17:05:47 -0500

    changes

--- src/router/router.js
+++ src/router/router.js
@@ -4,36 +4,44 @@ import NPFPage from "../modules/shared/pages/NoPageFound.vue";
 const routes = [
   {
     path: "/",
-    redirect: "/home",
+    redirect: "{name: 'pokemon'}",
   },
   {
-    path: "/home",
-    name:'home',
-    component: () =>
-      import(
-        /* webpackChunkName: "ListPage" */ "../modules/pokemon/pages/ListPage.vue"
-      ),
+    path: "/pokemon",
+    name:'pokemon',
+    component: () => import("../modules/pokemon/layouts/PokemonLayout.vue"),
+    children: [
+      {
+        path: "",
+        name: "pokemon-home",
+        component: () =>
+          import(
+            /* webpackChunkName: "ListPage" */ "../modules/pokemon/pages/ListPage.vue"
+          ),
+      },
+      {
+        path: "about",
+        name: "pokemon-about",
+        component: () =>
+          import(
+            /* webpackChunkName: "AboutPage" */ "../modules/pokemon/pages/AboutPage.vue"
+          ),
+      },
+      {
+        path: ":id",
+        name: "pokemon-id",
+        props: (route) => {
+          const id = Number(route.params.id);
+          return isNaN(id) ? { id: 1 } : { id };
+        },
+        component: () =>
+          import(
+            /* webpackChunkName: "PokemonPage" */ "../modules/pokemon/pages/PokemonPage.vue"
+          ),
+      },
+    ],
   },
-  {
-    path: "/about",
-    name:'about',
-    component: () =>
-      import(
-        /* webpackChunkName: "AboutPage" */ "../modules/pokemon/pages/AboutPage.vue"
-      ),
-  },
-  {
-    path: "/:id",
-    name: "pokemon-id",
-    props: (route) => {
-      const id = Number(route.params.id);
-      return isNaN(id) ? { id: 1 } : { id };
-    },
-    component: () =>
-      import(
-        /* webpackChunkName: "PokemonPage" */ "../modules/pokemon/pages/PokemonPage.vue"
-      ),
-  },
+
   { path: "/:pathMatch(.*)*", component: NPFPage },
 ];
 
